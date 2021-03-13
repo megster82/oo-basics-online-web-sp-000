53b8ef721ca9cabf53953f585ef42f7a4ac0e1eb
meaghan jennings <meaghanljennings@gmail.com>
2021-03-13 17:43:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+# Make your shoe class here!
+>>>>>>> c1b56796c3a6f24e694ff2d23e219e65d250a646
 class Shoe
   def initialize(brand)
     @brand = brand
   end 
+<<<<<<< HEAD
   
   def brand
     @brand
@@ -14,4 +19,11 @@ class Shoe
     puts "Your shoe is as good as new!"
   end 
   
+=======
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def cobble
+    @cobble = cobble
+  end 
+>>>>>>> c1b56796c3a6f24e694ff2d23e219e65d250a646
 end
